5d13ab2d5ecf541d1de3d9f452ad8f53292d4428
Amrit Singh <54405388+asingh1092@users.noreply.github.com>
2024-03-29 00:24:31 -0400

    created frontend to md to html backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Box, Container, TextField, Button, createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import axios from 'axios';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+  const [html, setHtml] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const convert = () => {
+    axios.post('http://localhost:8080/convert', markdown, {
+         headers: {
+           'Content-Type': 'text/plain'
+         }
+       })
+    .then(response => {
+    console.log(response)
+      setHtml(response.data);
+    })
+    .catch(error => {
+      console.error('Error converting Markdown to HTML:', error);
+    });
+  };
+
+  const handleMarkdownChange = (event) => {
+    setMarkdown(event.target.value);
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <h1>Markdown to HTML Converter</h1>
+         <Button variant="contained" onClick={toggleDarkMode}>
+                  {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+        </Button>
+        <TextField
+          label="Enter Markdown"
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={10}
+          value={markdown}
+          onChange={handleMarkdownChange}
+          margin="normal"
+        />
+        <Button variant="contained" color="primary" onClick={convert}>
+          Convert
+        </Button>
+         <h2>Converted HTML</h2>
+        {html && (
+           <Box mt={2} border={1} borderColor="grey.400" borderRadius={1} p={2}>
+             <div dangerouslySetInnerHTML={{ __html: html }} />
+           </Box>
+        )}
+      </Container>
+    </ThemeProvider>
   );
 }
 
